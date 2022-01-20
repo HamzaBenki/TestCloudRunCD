f9f52a964039373126b2398186758334fda06787
hamza benkirane <hamza.benkirane@hec.edu>
2022-01-20 20:17:43 +0100

    added predict function

--- api.py
+++ api.py
@@ -1,32 +1,42 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import joblib
 import pandas as pd
-import numpy as np
-import datetime
+from datetime import datetime
 import pytz
+import joblib
+import numpy as np
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+
 @app.get("/")
-def root():
-    return "Hello from Cloud Run CD"
+def index():
+    return {"greeting": "Hello world"}
+
 
 @app.get("/predict")
 def predict(pickup_datetime, pickup_longitude, pickup_latitude,
             dropoff_longitude, dropoff_latitude, passenger_count):
-   # create a datetime object from the user provided datetime
+    # create a datetime object from the user provided datetime
     input_datetime = pickup_datetime
     input_datetime = datetime.strptime(input_datetime, "%Y-%m-%d %H:%M:%S")
 
-    #localize the user datetime with NYC timezone
+    # localize the user datetime with NYC timezone
     eastern = pytz.timezone("US/Eastern")
     localized_pickup_datetime = eastern.localize(input_datetime, is_dst=None)
 
-# localize the datetime to UTC
+    # localize the datetime to UTC
     utc_pickup_datetime = localized_pickup_datetime.astimezone(pytz.utc)
 
-# format the datetime
+    # format the datetime
     formatted_pickup_datetime = utc_pickup_datetime.strftime(
         "%Y-%m-%d %H:%M:%S UTC")
 
@@ -44,12 +54,10 @@ def predict(pickup_datetime, pickup_longitude, pickup_latitude,
     X_pred_DataFrame['pickup_datetime'] = X_pred_DataFrame[
         'pickup_datetime'].astype(object)
 
-#Loading the saved model with joblib
+    #Loading the saved model with joblib
     model = joblib.load('model.joblib')
 
-#make prediction
+    #make prediction
     prediction = model.predict(X_pred_DataFrame)[0]
 
-    return {
-        'prediction':prediction
-        }
+    return {'prediction': prediction}
